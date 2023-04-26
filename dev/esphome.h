d98200e82f5b953e0a24e122bdbe94c4031d0833
Joe Sadusk <joe@sadusk.com>
2023-04-26 01:10:23 -0700

    Initial commit. Might be a working version, haven't tried it yet. Attempted without success to get a clangd config working

---
+++ dev/esphome.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//dummy header to make completion work
+
+#include <esphome/core/component.h>
+#include <esphome/components/api/custom_api_device.h>
+
---
+++ keypad_esp.h
@@ -0,0 +1,58 @@
+#include "esphome.h"
+#include <array>
+
+class KeypadComponent : public Component, public CustomAPIDevice {
+public:
+    const int numCols = 3;
+    const int numRows = 4;
+    const std::array<int, 3> columnPins {4, 0, 2};
+    const std::array<int 4> rowPins {5, 14, 12, 13};
+    const int buzzPin 15;
+    const int ledPin 1;
+    const std::array<const std::array<char, 3>, 4> keys {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
+        {'*', '0', '#'}
+    };
+    void setup() override {
+        for (const int pin : columnPins) {
+            pinMode(pin, OUTPUT);
+            digitalWrite(pin, LOW);
+        }
+        for (const int pin : rowPins) {
+            pinMode(pin, INPUT);
+        }
+    }
+
+    void loop() override {
+        std::string keys;
+        for (const size_t col = 0; i < columnPins.size(); ++col) {
+            const int colPin = columnPins[col];
+            digitalWrite(colPin, HIGH);
+
+            for (const size_t row = 0; i < rowPins.size(); ++row) {
+                const int rowPin = rowPins[row];
+                
+                if (digitalRead(rowPin)) {
+                    keys.push_back(keys[row][col]);
+                }
+            }
+            digitalWrite(colPin, LOW);
+        }
+
+        if (keys.size() != 1) {
+            return;
+        }
+
+        code.push_back(keys.front());
+        if (code.size() == 4) {
+            fire_homeassistant_event("keypad", {{"code", code}});
+            code.clear();
+        }
+    }
+
+private:
+    std::string code;
+    
+};
